a50c212db2a7a99870975bafb8a9c7d789ce7cc4
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-18 12:32:48 +0200

    Apply redirect in functie de user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,14 +25,7 @@
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-start mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
@@ -42,6 +35,14 @@
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
+            </div>
+            <div class="flex items-center justify-end mt-2">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Don\'t have an account?') }}
+                    </a>
+                @endif
+
             </div>
         </form>
     </x-jet-authentication-card>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -54,7 +54,7 @@
                 </div>
             @endif
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-center mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
