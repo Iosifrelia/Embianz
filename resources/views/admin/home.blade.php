24ad21e03c675290c234c51acdac2cd3e433b1b1
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-21 17:06:21 +0200

    update

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,2 +1,15 @@
-<x-app-layout>
-</x-app-layout>
+<x-dashboardheader />
+<body>
+  <head>
+    <nav>
+      <ul>
+        <li>home</li>
+        <li>products</li>
+        <li>details</li>
+        <li><x-app-layout>
+        </x-app-layout>
+      </li>
+      </ul>
+    </nav>
+  </head>
+</body>
