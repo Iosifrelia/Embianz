93ba622d2e4aede39ab581fa0452f2584ea21593
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-22 08:56:15 +0200

    make sass work

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,9 +1,9 @@
-<x-dashboardheader />
+{{-- <x-dashboardheader />
 <body>
   <head>
     <nav>
       <ul>
-        <li>home</li>
+        <li><a href="">Home</a></li>
         <li>products</li>
         <li>details</li>
         <li><x-app-layout>
@@ -13,3 +13,17 @@
     </nav>
   </head>
 </body>
+</html> --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="dist\css\main.css">
+  <title>Test</title>
+</head>
+<body>
+  <a  href="#">Test</a>
+</body>
+</html>
