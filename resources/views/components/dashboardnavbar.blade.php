005994c219beff8f4f1d59a594e96eeae8099e31
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-24 16:54:56 +0200

    last version / cc

--- resources/views/components/dashboardnavbar.blade.php
+++ resources/views/components/dashboardnavbar.blade.php
@@ -2,8 +2,13 @@
     <div class="container">
         <h1 class="site-title">Dashboard</h1>
         <ul class="display-f">
-            <li class="ml-1"><a href="#">Test</a></li>
-            <li class="ml-1"><a href="#">Test</a></li>
+            <li class="ml-1">
+                <a href="#">Test</a>
+            </li>
+            <li class="ml-1">
+                <x-app-layout>
+            </x-app-layout>
+        </li>
         </ul>
     </div>
 </nav>
