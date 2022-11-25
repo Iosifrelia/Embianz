9df6c68dedca06f80051694471dd118ad48edbef
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-25 13:06:21 +0200

    add sidebar section part 1

--- resources/views/components/dashboardnavbar.blade.php
+++ resources/views/components/dashboardnavbar.blade.php
@@ -1,9 +1,14 @@
-<nav class="navbar-primary">
-    
+<nav class="navbar-gray">
         <h1 class="site-title">Dashboard logo</h1>
+        <div><form action="" method="post">
+            <input type="text" name="" id="">
+            <input type="button" value="Submit" class="btn-secondary br-md">
+            </form>
+        </div>
         <ul class="display-f">
             <li class="ml-1">
-            {{$user}}
+                <button class="btn-white br-xs">{{$user}}</button>
+            
             </li>
         </ul> 
 </nav>
