f6f0f2c67386cae426c63cbe87008dcbf6f31275
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-25 16:57:03 +0200

    last

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,4 +1,7 @@
 <x-dashboardheader />
 <x-dashboardnavbar />
 <x-dashboardsidebar />
+{{-- Page content start --}}
+
+{{-- page content end --}}
 <x-dashboardfooter />
--- resources/views/components/dashboardnavbar.blade.php
+++ resources/views/components/dashboardnavbar.blade.php
@@ -1,13 +1,13 @@
-<nav class="navbar-gray">
+<nav class="navbar-bg">
         <h1 class="site-title">Dashboard logo</h1>
         <div><form action="" method="post">
-            <input type="text" name="" id="">
+            <input type="text" name="" id="" class="mr-2">
             <input type="button" value="Submit" class="btn-secondary br-md">
             </form>
         </div>
         <ul class="display-f">
             <li class="ml-1">
-                <button class="btn-white br-xs">{{$user}}</button>
+                <button class="btn-white br-xs o-80">{{$user}}</button>
             
             </li>
         </ul> 
