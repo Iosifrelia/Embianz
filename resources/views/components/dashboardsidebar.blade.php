7b9cee814c659201491c83f2f179c34e07039002
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-28 16:59:49 +0200

    last update

--- resources/views/components/dashboardsidebar.blade.php
+++ resources/views/components/dashboardsidebar.blade.php
@@ -1,34 +1,37 @@
-<div class="sidebar p-2">
+<div class="sidebar">
   <ul class="menu p-2">
-    <li class="list p-2 font-lg active">
+    <li class="list p-2 pb-3 font-lg active">
       <a href="#" class="link">
-        <img src="images/icon-01.svg" alt="icon" />
+        <img class="icon mr-2" src="/images/dashboardimg.webp" alt="icon">
         <span>Dashboard</span>
       </a>
     </li>
-    <li class="list p-2 font-lg">
+    <li class="list p-2 pb-3 font-lg">
       <a href="#" class="link">
-        <img src="images/icon-02.svg" alt="icon" />
+        
         <span>Category</span>
       </a>
     </li>
-    <li class="list p-2 font-lg">
+    <li class="list p-2 pb-3 font-lg">
       <a href="#" class="link">
-        <img src="images/icon-03.svg" alt="icon" />
+        
         <span>Products</span>
       </a>
     </li>
-    <li class="list p-2 font-lg">
+    <li class="list p-2 pb-3 font-lg">
       <a href="#" class="link">
-        <img src="images/icon-04.svg" alt="icon" />
+        
         <span>Orders</span>
       </a>
     </li>
-    <li class="list p-2 font-lg">
+    <li class="list p-2 pb-3 font-lg">
       <a href="#" class="settings">
-        <img src="images/icon-05.svg" alt="icon" />
+        
         <span>Settings</span>
       </a>
     </li>
   </ul>
+  {{-- <div class="copyright">
+    <span>Copyright Embianz</span>
+  </div> --}}
 </div>
