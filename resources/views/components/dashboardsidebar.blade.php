69b1ad8b18aee73249dcb861a638bd2113b5359c
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-28 13:11:43 +0200

    add sidebar items

--- resources/views/components/dashboardsidebar.blade.php
+++ resources/views/components/dashboardsidebar.blade.php
@@ -1,30 +1,30 @@
 <div class="sidebar p-2">
-  <ul class="menu">
-    <li class="list active">
+  <ul class="menu p-2">
+    <li class="list p-2 font-lg active">
       <a href="#" class="link">
         <img src="images/icon-01.svg" alt="icon" />
         <span>Dashboard</span>
       </a>
     </li>
-    <li class="list">
+    <li class="list p-2 font-lg">
       <a href="#" class="link">
         <img src="images/icon-02.svg" alt="icon" />
         <span>Category</span>
       </a>
     </li>
-    <li class="list">
+    <li class="list p-2 font-lg">
       <a href="#" class="link">
         <img src="images/icon-03.svg" alt="icon" />
         <span>Products</span>
       </a>
     </li>
-    <li class="list">
+    <li class="list p-2 font-lg">
       <a href="#" class="link">
         <img src="images/icon-04.svg" alt="icon" />
         <span>Orders</span>
       </a>
     </li>
-    <li class="list">
+    <li class="list p-2 font-lg">
       <a href="#" class="settings">
         <img src="images/icon-05.svg" alt="icon" />
         <span>Settings</span>
