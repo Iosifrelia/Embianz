11f926499cc46a800cc62568aa59fd31b145d1fc
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-29 16:51:31 +0200

    dropdown sistem

--- resources/views/components/dashboardnavbar.blade.php
+++ resources/views/components/dashboardnavbar.blade.php
@@ -8,10 +8,14 @@
             <button class="bg-secondary-light-4 bg-hover-secondary-light-1 br-lg p-1" type="submit" value="search"><img src="/images/magnifier.svg" alt="search_lupe" class="p-0"></button>
             </form>
         </div>
-        <ul class="display-f">
-            <li class="ml-1">
+        <ul class="dropdown">
+            <li class="ml-1 ">
                 <button id="button_user" class="btn-secondary font-lg pb-1 pl-2 pt-1 br-xs o-80">{{$user}}<img src="/images/chevron-down.svg" alt="icon" class="ml-1"></button>
             {{-- add dropdown element // show profile and logout --}}
+            <div class="dropdown-content bg-secondary-light-4 font-lg">
+                <a class="p-1" href="#">{{__('Profile')}}</a>
+                <a class="p-1" href="#">{{__('Logout')}}</a>
+            </div>
             </li>
         </ul> 
 </nav>
