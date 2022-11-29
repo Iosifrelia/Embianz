01bfe6f9fa27ba74833642b958bd5cd9a376ea26
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-29 10:59:54 +0200

    last update sidebar

--- resources/views/components/dashboardsidebar.blade.php
+++ resources/views/components/dashboardsidebar.blade.php
@@ -1,33 +1,33 @@
 <div class="sidebar">
-  <ul class="menu p-2">
-    <li class="list p-2 pb-3 font-lg active">
+  <ul class="menu">
+    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg active">
       <a href="#" class="link">
-        <img class="icon  mr-2" src="/images/chart-bars.svg" alt="icon">
-        <span>Dashboard</span>
+        <img class="icon p-1 br-sm bg-sidebar-bg-light-1 mr-2" src="/images/chart-bars.svg" alt="icon">
+        <span class="text-sidebar-bg-light-6">Dashboard</span>
       </a>
     </li>
-    <li class="list p-2 pb-3 font-lg">
+    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg">
       <a href="#" class="link">
-        <img src="/images/inbox.svg" alt="icon" class="icon br-sm mr-2">
-        <span>Category</span>
+        <img src="/images/inbox.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1">
+        <span class="text-sidebar-bg-light-6">Category</span>
       </a>
     </li>
-    <li class="list p-2 pb-3 font-lg">
+    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg">
       <a href="#" class="link">
-        <img src="/images/store.svg" alt="" class="icon br-xs mr-2">
-        <span>Products</span>
+        <img src="/images/store.svg" alt="" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1">
+        <span class="text-sidebar-bg-light-6">Products</span>
       </a>
     </li>
-    <li class="list p-2 pb-3 font-lg">
+    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg">
       <a href="#" class="link">
-        <img src="/images/cart.svg" alt="" class="icon br-xs mr-2">
-        <span>Orders</span>
+        <img src="/images/cart.svg" alt="" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1">
+        <span class="text-sidebar-bg-light-6">Orders</span>
       </a>
     </li>
-    <li class="list p-2 pb-3 font-lg">
+    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg">
       <a href="#" class="settings">
-        <img src="/images/cog.svg" alt="" class="icon br-xs mr-2">
-        <span>Settings</span>
+        <img src="/images/cog.svg" alt="" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1">
+        <span class="text-sidebar-bg-light-6">Settings</span>
       </a>
     </li>
   </ul>
