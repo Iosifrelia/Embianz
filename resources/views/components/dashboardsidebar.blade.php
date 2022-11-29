04cd2b120024a55b6972fbc0068be70f185b93f2
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-29 09:40:19 +0200

    customise dashboard sidebar

--- resources/views/components/dashboardsidebar.blade.php
+++ resources/views/components/dashboardsidebar.blade.php
@@ -2,31 +2,31 @@
   <ul class="menu p-2">
     <li class="list p-2 pb-3 font-lg active">
       <a href="#" class="link">
-        <img class="icon mr-2" src="/images/dashboardimg.webp" alt="icon">
+        <img class="icon  mr-2" src="/images/chart-bars.svg" alt="icon">
         <span>Dashboard</span>
       </a>
     </li>
     <li class="list p-2 pb-3 font-lg">
       <a href="#" class="link">
-        <img src="/images/categoryicon.png" alt="icon" class="icon mr-2">
+        <img src="/images/inbox.svg" alt="icon" class="icon br-sm mr-2">
         <span>Category</span>
       </a>
     </li>
     <li class="list p-2 pb-3 font-lg">
       <a href="#" class="link">
-        
+        <img src="/images/store.svg" alt="" class="icon br-xs mr-2">
         <span>Products</span>
       </a>
     </li>
     <li class="list p-2 pb-3 font-lg">
       <a href="#" class="link">
-        
+        <img src="/images/cart.svg" alt="" class="icon br-xs mr-2">
         <span>Orders</span>
       </a>
     </li>
     <li class="list p-2 pb-3 font-lg">
       <a href="#" class="settings">
-        
+        <img src="/images/cog.svg" alt="" class="icon br-xs mr-2">
         <span>Settings</span>
       </a>
     </li>
