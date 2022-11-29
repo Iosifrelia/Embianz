52c2136d858fe5557a5d5dee4b549242e093158e
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-11-29 16:07:02 +0200

    make header & sidebar fixed

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,6 +2,10 @@
 <x-dashboardnavbar />
 <x-dashboardsidebar />
 {{-- Page content start --}}
-
+<section class="section-container bg-sidebar-bg-light-1">
+<div>
+  <h2>Test</h2>
+</div>
+</section>
 {{-- page content end --}}
 <x-dashboardfooter />
--- resources/views/components/dashboardnavbar.blade.php
+++ resources/views/components/dashboardnavbar.blade.php
@@ -1,16 +1,16 @@
-<nav class="navbar-bg">
+<nav class="navbar-bg pos-fix">
     <div class="logo">
         <img src="\images\rectangular_light.png"  alt="">
     </div>
         <div><form action="#" method="post" class="display-f">
             @csrf
-            <input type="text" name="input_search" id="input_search" class="mr-2 font-md pl-1 br-sm" placeholder="Search here">
-            <button class="bg-secondary-light-4 bg-hover-secondary-light-1 br-lg p-1" type="submit" value="search"><img src="/images/magnifier.svg" alt="search_lupe"></button>
+            <input type="text" name="input_search" id="input_search" class="mr-2 font-md p-1 br-sm" placeholder="Search here">
+            <button class="bg-secondary-light-4 bg-hover-secondary-light-1 br-lg p-1" type="submit" value="search"><img src="/images/magnifier.svg" alt="search_lupe" class="p-0"></button>
             </form>
         </div>
         <ul class="display-f">
             <li class="ml-1">
-                <button id="button_user" class="btn-bg-secondary-light-2 font-lg pb-1 pl-2 pt-1 br-xs o-80">{{$user}}<img src="/images/chevron-down.svg" alt="icon" class="ml-1 mr-1"> </button>
+                <button id="button_user" class="btn-secondary font-lg pb-1 pl-2 pt-1 br-xs o-80">{{$user}}<img src="/images/chevron-down.svg" alt="icon" class="ml-1"></button>
             {{-- add dropdown element // show profile and logout --}}
             </li>
         </ul> 
