24ce6146a0239a18a50ed6aedda9b44bc02bcce0
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-12-01 09:09:26 +0200

    modified sidebar

--- resources/views/components/dashboardsidebar.blade.php
+++ resources/views/components/dashboardsidebar.blade.php
@@ -1,33 +1,33 @@
 <div class="sidebar">
   <ul class="menu">
-    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg active">
+    <li class="list bg-hover-sidebar-bg-light-1 p-1 font-lg active">
       <a href="#" class="link">
         <img class="icon p-1 br-sm bg-sidebar-bg-light-1 mr-2" src="/images/chart-bars.svg" alt="icon">
-        <span class="text-sidebar-bg-light-6">{{__('Dashboard')}}</span>
+        <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Dashboard')}}</span>
       </a>
     </li>
-    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg">
+    <li class="list bg-hover-sidebar-bg-light-1 p-1 font-lg">
       <a href="#" class="link">
-        <img src="/images/inbox.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1">
-        <span class="text-sidebar-bg-light-6">{{__('Categories')}}</span>
+        <img src="/images/inbox.svg" alt="icon" class="icon  br-sm mr-2 bg-sidebar-bg-light-1">
+        <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Categories')}}</span>
       </a>
     </li>
-    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg">
+    <li class="list bg-hover-sidebar-bg-light-1 p-1 font-lg">
       <a href="#" class="link">
         <img src="/images/store.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1">
-        <span class="text-sidebar-bg-light-6">{{__('Products')}}</span>
+        <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Products')}}</span>
       </a>
     </li>
-    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg">
+    <li class="list bg-hover-sidebar-bg-light-1 p-1 font-lg">
       <a href="#" class="link">
         <img src="/images/cart.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1">
-        <span class="text-sidebar-bg-light-6">{{__('Orders')}}</span>
+        <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Orders')}}</span>
       </a>
     </li>
-    <li class="list bg-hover-sidebar-bg-light-1 p-2 font-lg">
+    <li class="list bg-hover-sidebar-bg-light-1 p-1 font-lg">
       <a href="#" class="settings">
-        <img src="/images/cog.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1">
-        <span class="text-sidebar-bg-light-6">{{__('Settings')}}</span>
+        <img src="/images/cog.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
+        <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Settings')}}</span>
       </a>
     </li>
   </ul>
