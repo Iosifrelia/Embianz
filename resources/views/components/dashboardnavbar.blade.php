75062461abf4a074dbd27176b389efdc1ad44bed
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2022-12-01 15:36:25 +0200

    last update

--- resources/views/components/dashboardnavbar.blade.php
+++ resources/views/components/dashboardnavbar.blade.php
@@ -1,20 +1,20 @@
 <nav class="navbar-bg pos-fix">
     <div class="logo">
-        <img src="\images\rectangular_light.png"  alt="">
+        <img src="/images/dashboard/navbar/rectangular_light.png"  alt="">
     </div>
         <div><form action="#" method="post" class="display-f" x-data>
             @csrf
-            <input type="text" name="input_search" id="input_search" class="mr-2 font-md p-1 br-sm" placeholder="Search here">
-            <button class="bg-secondary-light-4 bg-hover-secondary-light-1 br-lg p-1" type="submit" value="search"><img src="/images/magnifier.svg" alt="search_lupe" class="p-0"></button>
+            <input type="text" name="input_search" id="input_search" class="mr-2 font-md p-1 pl-3 bg-sidebar-bg-light-1 br-sm" placeholder="Search here...">
+            {{-- <button class="bg-secondary-light-4 bg-hover-secondary-light-1 br-lg p-1" type="submit" value="search"><img src="/images/magnifier.svg" alt="search_lupe" class="p-0"></button> --}}
             </form>
         </div>
         <ul class="dropdown">
             <li class="ml-1 ">
-                <button id="button_user" class="btn-secondary font-lg pb-1 pl-2 pt-1 br-xs o-80">{{$user}}<img src="/images/chevron-down.svg" alt="icon" class="ml-1"></button>
+                <button id="button_user" class="btn-secondary font-lg pb-1 pl-2 pt-1 br-xs o-80">{{$user}}<img src="/images/dashboard/navbar/chevron-down.svg" alt="icon" class="ml-1"></button>
             {{--dropdown element // show profile and logout --}}
             <div class="dropdown-content bg-secondary-light-4 font-lg">
-                <a class="p-1" href="{{ route('profile.show') }}">{{__('Profile')}} <img class="float-r" src="/images/user.svg" alt="user_icon"></a>
-                <a class="p-1" href="{{ route('logout') }}">{{__('Logout')}} <img class="float-r" src="/images/exit.svg" alt="user_icon"></a>
+                <a class="p-1" href="{{ route('profile.show') }}">{{__('Profile')}} <img class="float-r" src="/images/dashboard/navbar/user.svg" alt="user_icon"></a>
+                <a class="p-1" href="{{ route('logout') }}">{{__('Logout')}} <img class="float-r" src="/images/dashboard/navbar/exit.svg" alt="user_icon"></a>
             </div>
             {{-- end dropdown --}}
             </li>
--- resources/views/components/dashboardsidebar.blade.php
+++ resources/views/components/dashboardsidebar.blade.php
@@ -2,31 +2,31 @@
   <ul class="menu ">
     <li class="list bg-hover-sidebar-bg-light-2 p-1 font-sm active">
       <a href="{{url('redirect')}}" class="link">
-        <img class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2" src="/images/chart-bars.svg" alt="icon">
+        <img class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2" src="/images/dashboard/sidebar/chart-bars.svg" alt="icon">
         <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Dashboard')}}</span>
       </a>
     </li>
     <li class="list bg-hover-sidebar-bg-light-2 p-1 font-sm">
       <a href="{{url('category')}}" class="link">
-        <img src="/images/inbox.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
+        <img src="/images/dashboard/sidebar/inbox.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
         <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Categories')}}</span>
       </a>
     </li>
     <li class="list bg-hover-sidebar-bg-light-2 p-1 font-lg">
       <a href="#" class="link">
-        <img src="/images/store.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
+        <img src="/images/dashboard/sidebar/store.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
         <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Products')}}</span>
       </a>
     </li>
     <li class="list bg-hover-sidebar-bg-light-2 p-1 font-lg">
       <a href="#" class="link">
-        <img src="/images/cart.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
+        <img src="/images/dashboard/sidebar/cart.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
         <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Orders')}}</span>
       </a>
     </li>
     <li class="list bg-hover-sidebar-bg-light-2 p-1 font-lg">
       <a href="#" class="settings">
-        <img src="/images/cog.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
+        <img src="/images/dashboard/sidebar/cog.svg" alt="icon" class="icon p-1 br-sm mr-2 bg-sidebar-bg-light-1 bg-hover-secondary-light-2">
         <span class="text-sidebar-bg-light-6 text-hover-secondary-light-1">{{__('Settings')}}</span>
       </a>
     </li>
